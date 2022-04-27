39c5ba07fcbc65977c6a12d328046457a91d9def
Sina <ebr.sina@outlook.com>
2022-04-28 03:49:39 +0430

    fix errors

--- src/lib/styles/customTheme/fonts.ts
+++
@@ -1,8 +0,0 @@
-import type { DeepPartial, Theme } from "@chakra-ui/react";
-
-const fonts: DeepPartial<Theme["fonts"]> = {
-  body: "Outfit, sans-serif",
-  heading: "Outfit, serif",
-};
-
-export default fonts;
