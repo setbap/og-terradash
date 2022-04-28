9f4b6772462b031fb4ff7af277aca73e6897ae91
Sina <ebr.sina@outlook.com>
2022-04-28 06:29:42 +0430

    change image size

--- src/pages/api/anchor.tsx
+++ src/pages/api/anchor.tsx
@@ -6,8 +6,8 @@ import { withOGImage } from "next-api-og-image";
 export default withOGImage({
   type: 'png',
   quality: undefined,
-  width: 1024,
-  height: 1024,
+  width: 1200,
+  height: 630,
   template: {
     react: ({ totalDeposits, totalBorrowed, currentAPY, yieldReserve }) => (
       <>
@@ -21,59 +21,61 @@ export default withOGImage({
             rel="stylesheet"
           />
         </head>
-        <div className="min-h-screen bg-black p-20 flex flex-col divide-y justify-center gap-4">
-          <div className="flex justify-between">
-            <div className="flex">
-              <img alt="Anchor Icon" width="48" height="48" className="mr-4" src="https://app.anchorprotocol.com/assets/LogoTerra.114ab559.svg" />
-              <h1 className="text-6xl font-bold text-gray-300 leading-tight">Anchor </h1>
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <div style={{ width: '1200px', height: '630px' }} className="mx-auto bg-black p-16 flex flex-col divide-y justify-center gap-4">
+            <div className="flex justify-between">
+              <div className="flex">
+                <img alt="Anchor Icon" width="80" height="80" className="mr-4" src="https://app.anchorprotocol.com/assets/LogoTerra.114ab559.svg" />
+                <h1 className="text-8xl font-bold text-gray-300 leading-tight">Anchor </h1>
+              </div>
+              <img alt="TerraDash Icon" className="h-28 w-28" src="https://cdn.discordapp.com/attachments/964167275064197160/967881200570101810/Group_15.png" />
             </div>
-            <img alt="TerraDash Icon" className="h-20 w-20" src="https://cdn.discordapp.com/attachments/964167275064197160/967881200570101810/Group_15.png" />
-          </div>
-          <span>
-            <p className="font-medium leading-normal text-2xl text-gray-500 pt-4">
-              According our research
-              Total Deposits in Anchor is
-              <span className="text-green-400">&nbsp; {
-                millify(+totalDeposits, {
-                  precision: 2,
-                  decimalSeparator: ".",
-                })
-              } UST&nbsp;</span>
-              and Total Borrow Amount
-              <span className="text-red-400">&nbsp;
-                {
-                  millify(+totalBorrowed, {
+            <span>
+              <p className="font-medium leading-normal text-5xl text-gray-500 pt-4">
+                According our research
+                Total Deposits in Anchor is
+                <span className="text-green-400">&nbsp; {
+                  millify(+totalDeposits, {
                     precision: 2,
                     decimalSeparator: ".",
                   })
-                }&nbsp;UST&nbsp;</span>.
-              In Earn Side Anchor has
-              <span className="text-green-400">&nbsp;
-                {
-                  millify(+currentAPY, {
-                    precision: 2,
-                    decimalSeparator: ".",
-                  })
-                }
-                % APY&nbsp;</span>
-              on UST and has
-              <span className="text-green-400">&nbsp;
-                {
-                  millify(+yieldReserve, {
-                    precision: 2,
-                    decimalSeparator: ".",
-                  })
-                }
-                &nbsp;UST&nbsp;</span>
-              Yield Reserve.
-              <br />
-              <span className="text-base w-full pt-2 text-left">
-                for more information you can click this image or go to <span className="underline text-blue-300">TerraDash.Vercel.app</span>
-              </span>
-              <br />
+                } UST&nbsp;</span>
+                and Total Borrow Amount
+                <span className="text-red-400">&nbsp;
+                  {
+                    millify(+totalBorrowed, {
+                      precision: 2,
+                      decimalSeparator: ".",
+                    })
+                  }&nbsp;UST&nbsp;</span>.
+                In Earn Side Anchor has
+                <span className="text-green-400">&nbsp;
+                  {
+                    millify(+currentAPY, {
+                      precision: 2,
+                      decimalSeparator: ".",
+                    })
+                  }
+                  % APY&nbsp;</span>
+                on UST and has
+                <span className="text-green-400">&nbsp;
+                  {
+                    millify(+yieldReserve, {
+                      precision: 2,
+                      decimalSeparator: ".",
+                    })
+                  }
+                  &nbsp;UST&nbsp;</span>
+                Yield Reserve.
+                <br className="mb-2" />
+                <span className="text-base w-full mt-3 text-left">
+                  for more information you can click this image or go to <span className="underline text-blue-300">TerraDash.Vercel.app</span>
+                </span>
+                <br />
 
-            </p>
-          </span>
+              </p>
+            </span>
+          </div>
         </div>
       </>
     ),
